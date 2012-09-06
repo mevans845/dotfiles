d7b9fef025c5397dde9d171531cb70b1139cbadb
Shrey Banga <banga.shrey@gmail.com>
2012-09-05 22:17:05 -0400

    Final touches to powerline and added screenshots

--- powerline-bash.py
+++ powerline-bash.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import getpass
 import os
 import subprocess
 import sys
 
 class Powerline:
+    separator = '⮀'
+    separator_thin="⮁"
+    ESC = '\e'
+    LSQ = '\['
+    RSQ = '\]'
+    clear_fg = LSQ + ESC + '[38;0m' + RSQ
+    clear_bg = LSQ + ESC + '[48;0m' + RSQ
+
     def __init__(self):
         self.segments = []
-        self.separator = '⮀'
-        self.ESC = '\e'
-        self.LSQ = '\\['
-        self.RSQ = '\\]'
-        self.clear_fg = self.LSQ + self.ESC + '[38;0m' + self.RSQ
-        self.clear_bg = self.LSQ + self.ESC + '[48;0m' + self.RSQ
 
-    def append(self, content, fg, bg):
-        segment = {'content': str(content), 'fg': str(fg), 'bg': str(bg)}
-        self.segments.append(segment)
+    def append(self, content, fg, bg, separator=None, separator_fg=None):
+      if separator == None:
+        separator = self.separator
+      if separator_fg == None:
+        separator_fg = bg
+      segment = {
+          'content': str(content),
+          'fg': str(fg),
+          'bg': str(bg),
+          'separator': str(separator),
+          'separator_fg': str(separator_fg)
+          }
+      self.segments.append(segment)
 
     def color(self, prefix, code):
         return self.LSQ + self.ESC + '[' + prefix + ';5;' + code + 'm' + self.RSQ
@@ -36,34 +47,47 @@ class Powerline:
             s = self.segments[i]
             ns = self.segments[i+1]
             line += self.fgcolor(s['fg']) + self.bgcolor(s['bg']) + s['content']
-            line += self.fgcolor(s['bg']) + self.bgcolor(ns['bg']) + self.separator
+            line += self.fgcolor(s['separator_fg']) + self.bgcolor(ns['bg']) + s['separator']
             i += 1
         s = self.segments[i]
         line += self.fgcolor(s['fg']) + self.bgcolor(s['bg']) + s['content']
-        line += self.clear_bg + self.fgcolor(s['bg']) + self.separator + self.clear_fg
+        line += self.clear_bg + self.fgcolor(s['separator_fg']) + s['separator'] + self.clear_fg
         return line
 
-def get_git_branch():
+def add_git_segment(powerline):
     try:
-        output = subprocess.check_output("git branch 2> /dev/null | grep -e '\*'", shell=True)
-        return output[2:].rstrip()
+        branch = subprocess.check_output(
+            "git branch 2> /dev/null | grep -e '\*'",
+            shell=True).rstrip()[2:]
+        p.append(' ' + branch + ' ', 22, 148)
     except subprocess.CalledProcessError:
-        return ''
+      pass
+
+# Show working directory with fancy separators
+def add_cwd_segment(powerline):
+    #p.append(' \w ', 15, 237)
+    home = os.getenv('HOME')
+    cwd = os.getcwd()
+    if cwd.find(home) == 0:
+      cwd = cwd.replace(home, '~', 1)
+    names = cwd.split('/')
+    for n in names[:-1]:
+      powerline.append(' ' + n + ' ', 250, 237, Powerline.separator_thin, 244)
+    powerline.append(' ' + names[-1] + ' ', 254, 237)
+
+def add_root_indicator(powerline, error):
+    bg = 236
+    fg = 15
+    if int(error) != 0:
+        fg = 15
+        bg = 161
+    p.append(' \$ ', fg, bg)
 
 if __name__ == '__main__':
-    last_cmd_error = sys.argv[1]
-
     p = Powerline()
-    p.append(' ' + getpass.getuser().upper() + ' ', 250, 238)
-    p.append(' \w ', 15, 237)
-
-    git_branch = get_git_branch()
-    if len(git_branch) > 0:
-        p.append(' ' + git_branch + ' ', 0, 148)
-
-    if int(last_cmd_error) == 0:
-        p.append('\$', 7, 239)
-    else:
-        p.append('\$', 237, 130)
-
+    p.append(' \u ', 250, 240)
+    p.append(' \h ', 250, 238)
+    add_cwd_segment(p)
+    add_git_segment(p)
+    add_root_indicator(p, sys.argv[1] if len(sys.argv) > 1 else 0)
     print p.draw(),
